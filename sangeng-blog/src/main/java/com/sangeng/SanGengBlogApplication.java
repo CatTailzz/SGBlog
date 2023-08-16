3308c3cd8eb5c41ce7a8c57c35f3a11ce43743b1
zsjhd <zsj1669677544@qq.com>
2023-08-16 16:18:45 +0800

    Initial commit

---
+++ sangeng-blog/src/main/java/com/sangeng/SanGengBlogApplication.java
@@ -0,0 +1,17 @@
+package com.sangeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zsj
+ * @Description :
+ * @Create on : 2023/8/16 16:04
+ **/
+@SpringBootApplication
+
+public class SanGengBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SanGengBlogApplication.class, args);
+    }
+}
---
+++ sangeng-framework/src/main/java/com/sangeng/Main.java
@@ -0,0 +1,12 @@
+package com.sangeng;
+
+/**
+ * @author zsj
+ * @Description :
+ * @Create on : 2023/8/10 15:19
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
